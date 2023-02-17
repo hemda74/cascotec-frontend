33898bffcfc15a7af3da5d7c6b0def51bacfb5f2
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-17 18:22:44 +0200

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -59,11 +59,11 @@ const Footer = () => {
                                     <span className={`input-group-text ${styels.subsIcon}`}><BsFillArrowUpRightCircleFill/></span>
                                 </div>
                             <div className={`m-auto mt-2`}>
-                                <a href="https://twitter.com/AhmedHemdan72" target="_blank" className={` me-3 ms-3 ${styels.footerA}`} >< BsTwitter size={25}/></a>
-                                <a href="https://www.facebook.com/profile.php?id=100009361588650" target="_blank" className={`me-3 ms-3 ${styels.footerA}`}>< BsFacebook size={25}/></a>
-                                <a href={'https://www.linkedin.com/in/ahmed-hemdan-69540820'} target="_blank" className={`me-3 ms-3  ${styels.footerA}`}> <BsLinkedin size={25}/></a>
-                                <a href={'https://www.linkedin.com/in/ahmed-hemdan-69540820'} target="_blank" className={`me-3 ms-3  ${styels.footerA}`}> <BsInstagram size={25}/></a>
-                                <a href="https://github.com/hemda74" target="_blank" className={`me-3 ms-3  ${styels.footerA}`}> <BsYoutube size={25}/></a>
+                                <a href="https://twitter.com/AhmedHemdan72" target="_blank" rel="noopener noreferrer" className={` me-3 ms-3 ${styels.footerA}`} >< BsTwitter size={25}/></a>
+                                <a href="https://www.facebook.com/profile.php?id=100009361588650" rel="noopener noreferrer" target="_blank" className={`me-3 ms-3 ${styels.footerA}`}>< BsFacebook size={25}/></a>
+                                <a href={'https://www.linkedin.com/in/ahmed-hemdan-69540820'}  rel="noopener noreferrer" target="_blank" className={`me-3 ms-3  ${styels.footerA}`}> <BsLinkedin size={25}/></a>
+                                <a href={'https://www.linkedin.com/in/ahmed-hemdan-69540820'} target="_blank" rel="noopener noreferrer" className={`me-3 ms-3  ${styels.footerA}`}> <BsInstagram size={25}/></a>
+                                <a href="https://github.com/hemda74" target="_blank" rel="noopener noreferrer" className={`me-3 ms-3  ${styels.footerA}`}> <BsYoutube size={25}/></a>
                             </div>
                         </div>
                     </div>
