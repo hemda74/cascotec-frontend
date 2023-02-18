87b2dd595270ba29c359d5255f7d7c17ce7b9d07
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-18 20:00:54 +0200

    try 7

--- app/components/BoardOfDirectors.tsx
+++ app/components/BoardOfDirectors.tsx
@@ -15,7 +15,7 @@ const BoardOfDirectors = () => {
         </div>
         <div className="row gy-5">
         <div className="col-xl-4 col-md-6 d-flex">
-        <Link href={'https://www.linkedin.com/in/ahmed-hemdan-695408201/'} target="_blank" rel="noreferrer">
+        <Link href={`https://www.linkedin.com/in/ahmed-hemdan-695408201/`} target="_blank" rel="noreferrer">
           <a>
         <div className={`${styles.card2}`}>
         <Image className={` w-90 ${styles.cardImage}`} src={mohamed}  alt=""/>
@@ -30,7 +30,7 @@ const BoardOfDirectors = () => {
       </Link>
     </div >
           <div className="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
-         <Link href={'https://www.linkedin.com/in/ahmed-hemdan-695408201/'} target="_blank" rel="noreferrer">
+         <Link href={`https://www.linkedin.com/in/ahmed-hemdan-695408201/`} target="_blank" rel="noreferrer">
           <a>
             <div className={`${styles.teamMember}`}>
               <div className={`${styles.teamImg}`}>
@@ -38,10 +38,10 @@ const BoardOfDirectors = () => {
               </div>
               <div className={`${styles.teamInfo}`}>
                 <div className={`${styles.socialMedia}`}>
-              <Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
-              <        Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
-              <        Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
-              <        Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
+              <Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
+              <Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
+              <Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
+              <Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link>
                         </div>
                 <div className={`${styles.nameTitle}`}>
                     <h4>Mohamed Hamdy</h4>
@@ -53,7 +53,7 @@ const BoardOfDirectors = () => {
             </Link>
           </div>
           <div className="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
-         <Link href={'https://www.linkedin.com/in/ahmed-hemdan-695408201/'} target="_blank" rel="noreferrer">
+         <Link href={`https://www.linkedin.com/in/ahmed-hemdan-695408201/`} target="_blank" rel="noreferrer">
           <a>
           <div className={`${styles.teamMember2}`}>
               <figure className={`${styles.teamMemberFigure}`}>
@@ -61,10 +61,10 @@ const BoardOfDirectors = () => {
                 <figcaption className={`${styles.figCaption}`}>
                   <p className='fs-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae asperiores mollitia.</p>
                   <ul className={`list-unstyled m-0 p-0 ${styles.figcaptionUl}`}>
-                    <li className={`${styles.figcaptionUlLi}`}><Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
-                    <li className={`${styles.figcaptionUlLi}`}><Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
-                    <li className={`${styles.figcaptionUlLi}`}><Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
-                    <li className={`${styles.figcaptionUlLi}`}><Link href={'/'} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
+                    <li className={`${styles.figcaptionUlLi}`}><Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
+                    <li className={`${styles.figcaptionUlLi}`}><Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
+                    <li className={`${styles.figcaptionUlLi}`}><Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
+                    <li className={`${styles.figcaptionUlLi}`}><Link href={`/`} className={`${styles.socialMediaLink}`}><a>< BsFacebook size={25} className={`${styles.iconI}`}/></a></Link></li>
                   </ul>
                 </figcaption>
               </figure>
