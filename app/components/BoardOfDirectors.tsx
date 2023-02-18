f1f8d19ba9cb90f5c0e1702cf6f28eec9c3c868b
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-18 19:06:40 +0200

    board of directors try 1

--- app/components/BoardOfDirectors.tsx
+++ app/components/BoardOfDirectors.tsx
@@ -1,34 +1,79 @@
 import React from 'react'
 import styles from '../styles/BoardOfDirectors.module.css'
 import mohamed from '../public/imagess/mohamedhamdy.jpg'
+import Link from 'next/link'
+import Image from 'next/image'
+import { BsFacebook } from 'react-icons/bs'
 const BoardOfDirectors = () => {
   return (
-    < >
-    <div className="row">
-    <div className={`card ${styles.cardOne}`}>
-  <div className={`card-bio ${styles.cardBio}`}>
-    <p className={`${styles.cardBioP}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-</div>
-<div className={`card ${styles.cardOne}`}>
-  <div className={`card-info ${styles.cardInfo}`}>
-    <p className={`title ${styles.cardTitle}`}>John Doe</p>
-    <p className={`subtitle ${styles.cardSubTitle}`}>Web Dev</p>
-  </div>
-  <div className={`card-bio ${styles.cardBio}`}>
-    <p className={`${styles.cardBioP}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-</div>
-<div className={`card ${styles.cardOne}`}>
-  <div className={`card-info ${styles.cardInfo}`}>
-    <p className={`title ${styles.cardTitle}`}>John Doe</p>
-    <p className={`subtitle ${styles.cardSubTitle}`}>Web Dev</p>
-  </div>
-  <div className={`card-bio ${styles.cardBio}`}>
-    <p className={`${styles.cardBioP}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-</div>
-</div>
+    <>
+   <section id="team" className={`${styles.boardOfDirc}`}>
+      <div  className="container" data-aos="fade-up">
+        <div className={`section-header text-center mt-5 ${styles.sectionHeader}`}>
+          <span className={`fs-3  ${styles.sctionTitle}`} >Our Team</span>
+          <p>Architecto nobis eos vel nam quidem vitae temporibus voluptates qui hic deserunt iusto omnis nam voluptas asperiores sequi tenetur dolores incidunt enim voluptatem magnam cumque fuga.</p>
+        </div>
+        <div className="row gy-5">
+        <div className="col-xl-4 col-md-6 d-flex">
+        <a href='https://www.linkedin.com/in/ahmed-hemdan-695408201/' target="_blank" rel="noreferrer">
+        <div className={`${styles.card2}`}>
+        <Image className={` w-90 ${styles.cardImage}`} src={mohamed}  alt=""/>
+          <div className={`${styles.cardoverlay}`}>
+            <div className={` text-center w-100 p-2" ${styles.overlayText}`}>
+            <h4>Mohamed Hamdy</h4>
+            <span className='text-white'>Chief Executive Officer</span>
+          </div>
+        </div>
+        </div>
+      </a>
+    </div >
+          <div className="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
+         <a href='https://www.linkedin.com/in/ahmed-hemdan-695408201/' target="_blank" rel="noreferrer">
+            <div className={`${styles.teamMember}`}>
+              <div className={`${styles.teamImg}`}>
+                <Image src={mohamed}  alt=""/>
+              </div>
+              <div className={`${styles.teamInfo}`}>
+                <div className={`${styles.socialMedia}`}>
+                <a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a>
+                <a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a>
+                <a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a>
+                <a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a>
+                </div>
+                <div className={`${styles.nameTitle}`}>
+                    <h4>Mohamed Hamdy</h4>
+                    <span className='text-white'>Chief Executive Officer</span>
+                </div>
+              </div>
+            </div>
+            </a>
+          </div>
+          <div className="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
+         <a href='https://www.linkedin.com/in/ahmed-hemdan-695408201/' target="_blank" rel="noreferrer">
+          <div className={`${styles.teamMember2}`}>
+              <figure className={`${styles.teamMemberFigure}`}>
+                <Image src={mohamed} alt="" className="img-responsive"/>
+                <figcaption className={`${styles.figCaption}`}>
+                  <p className='fs-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae asperiores mollitia.</p>
+                  <ul className={`list-unstyled m-0 p-0 ${styles.figcaptionUl}`}>
+                    <li className={`${styles.figcaptionUlLi}`}><a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a></li>
+                    <li className={`${styles.figcaptionUlLi}`}><a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a></li>
+                    <li className={`${styles.figcaptionUlLi}`}><a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a></li>
+                    <li className={`${styles.figcaptionUlLi}`}><a href="/" className={`${styles.socialMediaLink}`}>< BsFacebook size={25} className={`${styles.iconI}`}/></a></li>
+                  </ul>
+                </figcaption>
+              </figure>
+              <div className="text-center text-black fw-bold">
+              <h4 className='fw-bold' >Mohamed Hamdy</h4>
+              <p>Creative Director</p>
+              </div>
+            </div>
+            </a>
+            </div>
+            </div>
+            </div>
+    </section>
+    {/* <!-- End Team Section --> */}
     </>
   )
 }
--- app/components/PagesHeader.tsx
+++ app/components/PagesHeader.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import styles from '../styles/PagesHeader.module.css'
 const PagesHeader = () => {
   return (
-    <div className={`${styles.aboutHeader} d-flex `}>
+    <div className={`${styles.aboutHeader} d-flex  `}>
+      {/* <span className='text-white m-auto'> Team</span> */}
         </div>
   )
 }
