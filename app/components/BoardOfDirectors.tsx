8e50dabdaace6cbb213c1b2fb90affa1c5ca1f04
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-19 06:13:23 +0200

    bf

--- app/components/BoardOfDirectors.tsx
+++ app/components/BoardOfDirectors.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/BoardOfDirectors.module.css'
 import mohamed from '../public/imagess/mohamedhamdy.jpg'
 import Link from 'next/link'
 import Image from 'next/image'
-import { BsFacebook } from 'react-icons/bs'
+import egypt from '../public/imagess/eg-flag.gif'
 const BoardOfDirectors = () => {
   return (
     <>
@@ -16,9 +16,42 @@ const BoardOfDirectors = () => {
           <div className={`${styles.teamMember2}`}>
               <figure className={`${styles.teamMemberFigure}`}>
                 <Image src={mohamed} height={550} alt="" className="img-responsive"/>
-                <figcaption className={`${styles.figCaption}`}>
-                  <p className='fs-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae asperiores mollitia.</p>
-                  
+                <figcaption className={`${styles.figCaption} text-center`}>
+                  <p className='fs-3'>Mr. Ahmed Mahrous</p>
+                  <p className='fs-5'>Mr. Ahmed Mahrous</p>
+                  <Image src={egypt} width={400} height={150} className="m-auto"/>
+                </figcaption>
+              </figure>
+            </div>
+            </a>
+            </Link>
+            </div>
+            <div className="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
+         <Link href={`https://www.linkedin.com/in/ahmed-hemdan-695408201/`} target="_blank" rel="noreferrer">
+          <a>
+          <div className={`${styles.teamMember2}`}>
+              <figure className={`${styles.teamMemberFigure}`}>
+                <Image src={mohamed} height={550} alt="" className="img-responsive"/>
+                <figcaption className={`${styles.figCaption} text-center`}>
+                  <p className='fs-3'>Mr. Ahmed Mahrous</p>
+                  <p className='fs-5'>Mr. Ahmed Mahrous</p>
+                  <Image src={egypt} width={400} height={150} className="m-auto"/>
+                </figcaption>
+              </figure>
+            </div>
+            </a>
+            </Link>
+            </div>
+            <div className="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
+         <Link href={`https://www.linkedin.com/in/ahmed-hemdan-695408201/`} target="_blank" rel="noreferrer">
+          <a>
+          <div className={`${styles.teamMember2}`}>
+              <figure className={`${styles.teamMemberFigure}`}>
+                <Image src={mohamed} height={550} alt="" className="img-responsive"/>
+                <figcaption className={`${styles.figCaption} text-center`}>
+                  <p className='fs-3'>Mr. Ahmed Mahrous</p>
+                  <p className='fs-5'>Mr. Ahmed Mahrous</p>
+                  <Image src={egypt} width={400} height={150} className="m-auto"/>
                 </figcaption>
               </figure>
             </div>
