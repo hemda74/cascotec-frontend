4e7adc017c2abf6e0080816ae2918f754a434834
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-02-21 03:53:44 +0200

    update

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -57,8 +57,7 @@ const Login: NextPage = () => {
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-          </link>
+      <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       <title>CASCO</title>
    </Head>
    <NavBar/>
