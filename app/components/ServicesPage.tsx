3cb2c756cbb061fc797ec8d2f3035184136586a3
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-03 21:14:36 +0200

    handle services errors

--- app/components/ServicesPage.tsx
+++ app/components/ServicesPage.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 import styles from '../styles/ServicesPage.module.css'
 const ServicesPage = () => {
   return (
@@ -11,7 +12,7 @@ const ServicesPage = () => {
     <div className={`row gy-5  container justify-content-center align-items-center m-auto ${styles.formConatiner}`}>
     <div className="card_accreditation d-flex ">
                                     <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                     <div className={`${styles.accreditationThumbnail}`}>
                                         <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
@@ -28,12 +29,12 @@ const ServicesPage = () => {
                                           <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
                                                                             </div>
                                                                             <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                 </div>
                                 <div className="card_accreditation d-flex ">
                                     <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                     <div className={`${styles.accreditationThumbnail}`}>
                                         <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
@@ -50,12 +51,12 @@ const ServicesPage = () => {
                                           <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
                                                                             </div>
                                                                             <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                 </div>
                                 <div className="card_accreditation d-flex ">
                                     <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                     <div className={`${styles.accreditationThumbnail}`}>
                                         <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
@@ -69,15 +70,15 @@ const ServicesPage = () => {
                                         <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
                                         <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017
                                                   General requirements for the competence of testing and calibration laboratories. </p>
-                                          <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
+                                          <Link href="/login" target="_blank"  className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
                                                                             </div>
                                                                             <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                 </div>
                                 <div className="card_accreditation d-flex ">
                                     <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                     <div className={`${styles.accreditationThumbnail}`}>
                                         <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
@@ -94,7 +95,7 @@ const ServicesPage = () => {
                                           <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
                                                                             </div>
                                                                             <div >
-                                        <img className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
+                                        <Image className={`${styles.accreditationThumbnail2}`}src="https://gac.org.sa/wp-content/uploads/2022/05/1-Testing.jpg" alt=""/>
                                     </div>
                                 </div>
                               
