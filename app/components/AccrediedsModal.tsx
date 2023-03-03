e54836ce11d60fbe6267fe618065e063aeab390e
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-03 21:58:53 +0200

    Update Accredit Modal

--- app/components/AccrediedsModal.tsx
+++ app/components/AccrediedsModal.tsx
@@ -32,42 +32,42 @@ const AccrediedsModal = (props: Props) => {
               <p className="fs-5 me-2 ms-2 fw-semibold">Arab Shipbuilding and Repair Yard (ASRY)</p>
               <small className=' text-black-50 me-2 ms-2'>ACL 0021</small>
               <div className={`d-flex ${styles.imageConutry} me-2 ms-2`}>
-              <Image
-              className=' p-2'
-                src={uae}
-                width={33}
-                height={33}
-                alt='countryflage'
-                />
+                  <Image
+                    className=' p-2'
+                    src={uae}
+                    width={33}
+                    height={33}
+                    alt='countryflage'
+                  />
                   <span className='me-1 mt-1 '>
                     Emirates
-                    </span>
+                  </span>
+              </div>
+              <div className={`${styles.itemDownload} d-flex justify-content-between`}>
+                    <span className="title">Certificates</span>
+                    <div className="actions">
+                        <a target="_blank" href="https://api.gac.org.sa/public/uploads/cab_request/certificates/vSGcxUuVPTxz5aulhHsbnv5m5v2w3kQsj7pvndUYj2fo2qSScH7UFO3HDGOD1gEiLYdqkcxOIysNfMz1.pdf" rel="noreferrer" className="btn btn_icon grey">
+                            <AiFillEye/>
+                        </a>
+                        <a download="new" data-type="2"  className="btn btn_icon theme"  data-file="../public/HPC 0069.pdf" data-name="AC 10.11 v1.2 Accreditation certificate ACL0021.pdf">
+                            <AiOutlineDownload/>
+                        </a> 
+                          <a href="../public/HPC 0069.pdf"  download="newfilename"> </a>
                     </div>
-                    <div className={`${styles.itemDownload} d-flex    justify-content-between`}>
-                                <span className="title">Certificates</span>
-                                <div className="actions">
-                                    <a target="_blank" href="https://api.gac.org.sa/public/uploads/cab_request/certificates/vSGcxUuVPTxz5aulhHsbnv5m5v2w3kQsj7pvndUYj2fo2qSScH7UFO3HDGOD1gEiLYdqkcxOIysNfMz1.pdf" rel="noreferrer" className="btn btn_icon grey">
-                                    <AiFillEye/>
-                                    </a>
-                                     <a download="new" data-type="2"  className="btn btn_icon theme"  data-file="../public/HPC 0069.pdf" data-name="AC 10.11 v1.2 Accreditation certificate ACL0021.pdf">
-                                        <AiOutlineDownload/>
-                                     </a> 
-                                     <a href="../public/HPC 0069.pdf"  download="newfilename"> </a>
-                                </div>
-                            </div>
-                            <div className={`${styles.itemDownload2} d-flex    justify-content-between`}>
-                                <span className="title">Scope</span>
-                                <div className="actions">
-                                    <a target="_blank"  rel="noreferrer" href="https://api.gac.org.sa/public/uploads/cab_request/certificates/vSGcxUuVPTxz5aulhHsbnv5m5v2w3kQsj7pvndUYj2fo2qSScH7UFO3HDGOD1gEiLYdqkcxOIysNfMz1.pdf" className="btn btn_icon grey">
-                                    <AiFillEye/>
-                                    </a>
-                                     <a download="new" data-type="2" className="btn btn_icon theme"  data-file="../public/HPC 0069.pdf" data-name="AC 10.11 v1.2 Accreditation certificate ACL0021.pdf">
-                                        <AiOutlineDownload/>
-                                     </a> 
-                                     <a href="../public/HPC 0069.pdf" download="newfilename"> </a>
-                                </div>
-                            </div>
+                </div>
+                <div className={`${styles.itemDownload2} d-flex    justify-content-between`}>
+                    <span className="title">Scope</span>
+                    <div className="actions">
+                        <a target="_blank"  rel="noreferrer" href="https://api.gac.org.sa/public/uploads/cab_request/certificates/vSGcxUuVPTxz5aulhHsbnv5m5v2w3kQsj7pvndUYj2fo2qSScH7UFO3HDGOD1gEiLYdqkcxOIysNfMz1.pdf" className="btn btn_icon grey">
+                        <AiFillEye/>
+                        </a>
+                          <a download="new" data-type="2" className="btn btn_icon theme"  data-file="../public/HPC 0069.pdf" data-name="AC 10.11 v1.2 Accreditation certificate ACL0021.pdf">
+                            <AiOutlineDownload/>
+                          </a> 
+                          <a href="../public/HPC 0069.pdf" download="newfilename"> </a>
                     </div>
+                </div>
+            </div>
           </div>
         </div>
       </div>
