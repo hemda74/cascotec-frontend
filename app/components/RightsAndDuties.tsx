126c8675f13d0a4f9a93140eda6f3738edd5a0d7
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-13 23:49:47 +0200

    update rights and duties

--- app/components/RightsAndDuties.tsx
+++ app/components/RightsAndDuties.tsx
@@ -14,18 +14,17 @@ const RightsAndDuties = () => {
             <h6 className="text-white fw-semibold mb-4" data-trans="routeCASCORightsAndDuties">{`Home > About CASCO > Rights And Duties`}</h6>
             <h2 className={`text-white fw-bold  `} data-trans="CASCORightsAndDuties">{`CASCO’s Rights And Duties`}</h2>
         </div>
-        <div className="row mx-0 d-flex justify-content-around align-items-center">
-            <div className="col-lg-2"></div>
-            <div className={`card-body col-xl-3 col-lg-3 col-md-6 col-sm-12`}>
-              <span className='me-lg-5' data-trans="CASCOSRIGHTSANDDUTIES">{`CASCO'S RIGHTS AND DUTIES`}</span>
-                  <h4 className='fw-bold me-lg-5 mt-2' data-trans="CASCOSRights">{`CASCO'S Rights`}</h4>
-                  </div>
-                  <div className={`card-body col-xl-3 col-lg-3 col-md-6 col-sm-12`}></div>
-                  </div>
-                  <div className="col-lg-2"></div>
-       <div className="row mx-0 d-flex justify-content-around align-items-center">
-            <div className="col-lg-2"></div>
-            <div className={`card-body col-xl-6 col-lg-6 col-md-12 col-sm-12 p-4`}>
+        <div className="container">
+            <div className="row mx-0 d-flex justify-content-around align-items-center">
+                <div className={`card-body col-xl-3 col-lg-3 col-md-6 col-sm-12`}>
+                    <span className={`me-lg-5 ${styles.spanHead}`} data-trans="CASCOSRIGHTSANDDUTIES">{`CASCO'S RIGHTS AND DUTIES`}</span>
+                    <h4 className='fw-bold me-lg-5 mt-2' data-trans="CASCOSRights">{`CASCO'S Rights`}</h4>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row mx-0  d-flex justify-content-around align-items-center">
+                <div className={`card-body col-xl-8 col-lg-8 col-md-12 col-sm-12 p-4`}>
                     <ul className={`${styles.ulClass}`}>
                         <li className={`${styles.itemCheckTage} d-flex`}>
                             <p><FcCheckmark size={20} className=''/></p>
@@ -44,47 +43,49 @@ const RightsAndDuties = () => {
                             <span data-trans='ritghs4' className='me-2 ms-2'>{`To assess the management system of the CAB and require any document and access to location and facilities in relation to the scope of accreditation during the accreditation cycle of the CAB.`}</span>
                         </li>
                     </ul>          
+                </div>
             </div>
-            <div className="col-lg-2"></div>
         </div>
-        <div className={`row mx-0 d-flex justify-content-around align-items-center ${styles.ctA}`}>
-            <div className="col-lg-2"></div>
-            <div className={`card-body col-xl-3 col-lg-3 col-md-6 col-sm-12 `}>
-              <Image src={dutes} className={``} alt="dutes image"/>
-              </div>
-              <div className={`card-body col-xl-3 col-lg-3 col-md-6 col-sm-12 ${styles.dutesList}`}>
-              <h4 className='fw-bold  text-white me-5' data-trans='CASCOsduties'>{`CASCO’s duties`}</h4>
-              <ul className='list-unstyled mt-4'>
-                        <li className={`${styles.itemCheckTage} d-flex`}>
-                            <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
-                            <span className='me-2 ms-2 text-white' data-trans='duiets1'>{`To timely plan and conduct due assessments for the CABs to determine its competence`}</span>
-                        </li>
-                        <li className={`${styles.itemCheckTage} d-flex mt-4`}>
-                            <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
-                            <span  className='me-2 ms-2 text-white' data-trans='duiets2'>{`To ensure confidentiality of CAB’s information and their documents unless required by the law/regulation to disclose it in a way required.`}</span>
-                        </li>
-                        <li className={`${styles.itemCheckTage} d-flex mt-4`}>
-                            <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
-                            <span  className='me-2 ms-2 text-white' data-trans='duiets3'>{`GAC will make publicly available information about the current status of the accreditations that it has granted to the CABs, this information is updated regularly and in line with ISO/IEC 17011 requirements.`}</span>
-                        </li>
-                        <li className={`${styles.itemCheckTage} d-flex mt-4`}>
-                            <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
-                            <span  className='me-2 ms-2 text-white' data-trans='duiets4'>{`To ensure impartiality of its services`}</span>
-                        </li>
-                        <li className={`${styles.itemCheckTage} d-flex mt-4`}>
-                            <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
-                            <span  className='me-2 ms-2 text-white' data-trans='duiets5'>{`To inform its accredited CABs about any changes in the accreditation requirements and timelines as applicable, to ensure the implementation of changes it could be covered through next due assessment or by asking CABs to provide some immediate evidences of implementation.`}</span>
-                        </li>
-                        <li className={`${styles.itemCheckTage} d-flex mt-4`}>
-                            <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
-                            <span  className='me-2 ms-2 text-white' data-trans='duiets6'>{`To provide information about suitable ways to obtain tractability of measurement results in relation to the scope for which accreditation is provided. This is detailed in the Technical Note TN 2.0.`}</span>
-                        </li>
-                    </ul>  
-              </div>
-              <div className="col-lg-2"></div>
+        <div className={` ${styles.ctA} mb-5 `}>
+            <div className={`container `}>
+                <div className={`row mx-0 d-flex justify-content-around align-items-center `}>
+                    <div className={`card-body col-xl-6 col-lg-6 col-md-6 col-sm-12 `}>
+                        <Image src={dutes} className={``} alt="dutes image"/>
+                    </div>
+                    <div className={`card-body col-xl-6 col-lg-6 col-md-6 col-sm-12 ${styles.dutesList}`}>
+                        <h4 className='fw-bold text-white me-5' data-trans='CASCOsduties'>{`CASCO’s duties`}</h4>
+                        <ul className='list-unstyled mt-4'>
+                            <li className={`${styles.itemCheckTage} d-flex`}>
+                                <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
+                                <small className='me-2 ms-2 text-white' data-trans='duiets1'>{`To timely plan and conduct due assessments for the CABs to determine its competence`}</small>
+                            </li>
+                            <li className={`${styles.itemCheckTage} d-flex mt-4`}>
+                                <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
+                                <small className='me-2 ms-2 text-white' data-trans='duiets2'>{`To ensure confidentiality of CAB’s information and their documents unless required by the law/regulation to disclose it in a way required.`}</small>
+                            </li>
+                            <li className={`${styles.itemCheckTage} d-flex mt-4`}>
+                                <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
+                                <small className='me-2 ms-2 text-white' data-trans='duiets3'>{`GAC will make publicly available information about the current status of the accreditations that it has granted to the CABs, this information is updated regularly and in line with ISO/IEC 17011 requirements.`}</small>
+                            </li>
+                            <li className={`${styles.itemCheckTage} d-flex mt-4`}>
+                                <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
+                                <small className='me-2 ms-2 text-white' data-trans='duiets4'>{`To ensure impartiality of its services`}</small>
+                            </li>
+                            <li className={`${styles.itemCheckTage} d-flex mt-4`}>
+                                <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
+                                <small className='me-2 ms-2 text-white' data-trans='duiets5'>{`To inform its accredited CABs about any changes in the accreditation requirements and timelines as applicable, to ensure the implementation of changes it could be covered through next due assessment or by asking CABs to provide some immediate evidences of implementation.`}</small>
+                            </li>
+                            <li className={`${styles.itemCheckTage} d-flex mt-4`}>
+                                <p><GiCheckMark size={20} className={`${styles.iconsColor}`}/></p>
+                                <small className='me-2 ms-2 text-white' data-trans='duiets6'>{`To provide information about suitable ways to obtain tractability of measurement results in relation to the scope for which accreditation is provided. This is detailed in the Technical Note TN 2.0.`}</small>
+                            </li>
+                        </ul>  
+                    </div>
+                </div>
+            </div>
         </div>
-     </section>
-    </>
+    </section>
+</>
   )
 }
 
