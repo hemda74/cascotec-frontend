de1426c0a778a6cbf5bd0d370976944918489e48
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-13 17:12:30 +0200

    update about page

--- app/components/AboutPage1.tsx
+++ app/components/AboutPage1.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/AboutPage1.module.css'
-import{GiShakingHands,GiRead} from 'react-icons/gi'
-import {FaRegAddressCard} from 'react-icons/fa'
+import{GiShakingHands} from 'react-icons/gi'
 import {FcCheckmark} from 'react-icons/fc'
-import {RiBookmark3Line} from 'react-icons/ri'
 import card_objective from '../public/imagess/card_objective.jpg';
 import gsoLogo from '../public/imagess/GSO-Logo-300x93.png';
-import {TfiCheck} from 'react-icons/tfi'
+
 const About = () => {
   return (
     <>
@@ -44,30 +42,29 @@ const About = () => {
         <div className={`${styles.ctA}`}>
             <div className="row">
                 <div className="col-lg-5 ms-2 text-lg-center mt-5">
-                    <p className='fs-6 fw-semibold text-white' data-trans='SUCCESSINDICATORS'>SUCCESS INDICATORS</p>
-                    <p className='fs-1 text-white fw-bold' data-trans="OurAchievements">Our Achievements</p>
+                    <p className='fs-6 fw-semibold text-white me-2 ms-2' data-trans='SUCCESSINDICATORS'>SUCCESS INDICATORS</p>
+                    <p className='fs-1 text-white fw-bold me-3 ms-3' data-trans="OurAchievements">Our Achievements</p>
                 </div>
-                {/* <div className={` d-flex col-lg-6 cta-btn-container text-center row ${styles.ctaBtn}`}> */}
-                    <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 d-flex justify-content-start align-items-start flex-wrap">
-                    <div className={` d-flex col-lg-6 col-sm-12 justify-content-center align-items-center`}>
-                        <p className={`${styles.iconsColor}`}>
+                    <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 d-flex justify-content-start align-items-start flex-wrap row">
+                    <div className={` d-flex col-lg-6 col-sm-12 col-md-6 col-xs-12   justify-content-center align-items-center ${styles.achievementsDiv}`}>
+                        <p className={`  ${styles.iconsColor}`}>
                         <GiShakingHands  size={60}/>
                         </p>
-                        <div className='mt-2'>
-                        <p className=' text-white fs-4 fw-bold me-3 ms-3'>311</p>
-                        <small className=' text-white me-3 ms-3' data-trans="AccreditedBodiesAbout">Accredited Bodies</small>
+                        <div className='mt-1'>
+                        <p className=' text-white fs-4 fw-bold me-3 mb-0 ms-3'>311</p>
+                        <small className=' text-white ms-3 me-3 d-block mb-4 text-start' data-trans="AccreditedBodiesAbout">Accredited Bodies</small>
                         </div>
                     </div>
-                    <div className={` d-flex col-lg-6 col-sm-12 justify-content-center align-items-center`}>
+                    <div className={` d-flex col-lg-6 col-sm-12 justify-content-center align-items-center ${styles.achievementsDiv}`}>
                         <p className={`${styles.iconsColor}`}>
                         <GiShakingHands  size={60}/>
                         </p>
                         <div className='mt-2'>
                         <span className=' text-white fs-4 fw-bold ms-3 me-3'>1784</span>
-                        <small className='mt-2 text-white d-block mb-4 ms-3 me-3' data-trans="AssessorsandExperts">AssessorsandExperts</small>
+                        <small className='text-white ms-3 me-3 d-block mb-4 text-start' data-trans="AssessorsandExperts">AssessorsandExperts</small>
                         </div>
                     </div>
-                    <div className={`d-flex col-lg-6 d-flex justify-content-center align-items-center`}>
+                    <div className={`d-flex col-lg-6 d-flex justify-content-center align-items-center ${styles.achievementsDiv}`}>
                         <p className={`${styles.iconsColor} text-start `}>
                         <GiShakingHands  size={60}/>
                         </p>
@@ -76,13 +73,13 @@ const About = () => {
                         <small className=' text-white ms-3 me-3 d-block mb-4 text-start' data-trans="Traineesduring2022">{`2022's Trainees`}</small>
                         </div>
                     </div>
-                    <div className={` col-lg-6 d-flex justify-content-center align-items-center`}>
+                    <div className={` col-lg-6 d-flex justify-content-center align-items-center ${styles.achievementsDiv}`}>
                         <p className={`${styles.iconsColor}`}>
                         <GiShakingHands size={60}/>
                         </p>
-                        <div className='mt-2 ms-3 me-3'>
-                        <p className=' text-white fs-4 fw-bold mb-1 ms-3 me-3'>31</p>
-                        <small className='text-white' data-trans="CASCOAgreements">CASCO Agreements</small>
+                        <div className=' ms-3 me-3'>
+                        <p className=' text-white fs-4 fw-bold  ms-3 mb-0 me-3'>31</p>
+                        <small className='text-white mb-4' data-trans="CASCOAgreements">CASCO Agreements</small>
                         </div>
                     </div>
                 </div>
@@ -104,33 +101,33 @@ const About = () => {
                     The Ethics Committee is responsible for enforcing the Code of Ethics, receiving reports of unethical behavior, and ensuring compliance with anti-corruption laws.
                     `}
                 </p>
-                <div className={` d-flex justify-content-around align-items-center`}>
+                <div className={`d-flex justify-content-around align-items-center`}>
                 <Image  src={gsoLogo} alt={'hello'}/>
                 </div>
             </div>
             <div className="col-lg-2"></div>
-        </div>
             </div>
-            <div className={`card-body col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-3 ${styles.ethicCard2}`}>                   
-             <div className={`${styles.ethicCard3}`}>
+            </div>
+            <div className={`container col-xl-8 col-lg-8 col-md-10 col-sm-12 col-xs-12 mb-5 ${styles.ethicCard2}`}>                  
+             <div className={`${styles.budgetCard}`}>
                     <span className='fs-4 fw-bold' data-trans="budget">The budget of CASCO</span>
-                    <p className='mt-3 fs-5'data-trans="budgetP">{`CASCO shall have a separate budget, funded by the following sources`}</p>
-                    <ul className='-list-unstyled'>
+                    <p className='mt-1 fs-5'data-trans="budgetP">{`CASCO shall have a separate budget, funded by the following sources`}</p>
+                    <ul className='list-unstyled m-0 p-0'>
                     <li className={`${styles.itemCheckTage} d-flex`}>
-                    <p><FcCheckmark size={28} className=''/></p>
-                            <span data-trans="budgetP1" className='me-4 ms-4  fs-6'>{`Fees charged by CASCO in exchange for its services.`}</span>
+                    <p><FcCheckmark size={15} className=''/></p>
+                            <small data-trans="budgetP1" className='me-2 ms-2'>{`Fees charged by CASCO in exchange for its services.`}</small>
                         </li>	
                         <li className={`${styles.itemCheckTage} d-flex`}>
-                            <p><FcCheckmark size={28} className=''/></p>
-                            <span data-trans="budgetP2" className='me-4 ms-4  fs-6'>{`Financial contributions from Member States`}</span>
+                            <p><FcCheckmark size={15} className=''/></p>
+                            <small data-trans="budgetP2" className='me-2 ms-2'>{`Financial contributions from Member States`}</small>
                         </li>
-                        <li className={`${styles.itemCheckTage} d-flex`}>
-                            <p><FcCheckmark size={28} className=''/></p>
-                            <span data-trans="budgetP3" className='me-4 ms-4  fs-6'>{`Grants and donations that do not compromise the independence and impartiality of CASCO, approved by the Board of Directors.`}</span>
+                        <li className={`${styles.itemCheckTage2} d-flex  `}>
+                            <p><FcCheckmark size={15} className=''/></p>
+                            <small data-trans="budgetP3" className='me-2 ms-2 '>{`Grants and donations that do not compromise the independence and impartiality of CASCO, approved by the Board of Directors.`}</small>
                         </li>
-                        <li className={`${styles.itemCheckTage} d-flex`}>
-                            <p><FcCheckmark size={28} className=''/></p>
-                                <span data-trans="budgetP4" className='me-4 ms-4  fs-6'>{`Other sources determined by the Board of Directors.`}</span>
+                        <li className={`${styles.itemCheckTage} mt-1  d-flex`}>
+                            <p><FcCheckmark size={15} className=''/></p>
+                                <small data-trans="budgetP4" className='me-2 mt-1 ms-2'>{`Other sources determined by the Board of Directors.`}</small>
                         </li>
                     </ul>
             </div>
