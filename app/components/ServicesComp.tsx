c363d76fbc7e5ffa8b4b8b3d7ebb45d00856a1df
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-15 17:56:22 +0200

    update service component

--- app/components/ServicesComp.tsx
+++ app/components/ServicesComp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import halal from '../public/imagess/halal.jpg';
+import halal from '../public/images/accretied_bodies/first_images/9.jpg';
 import Image from 'next/image';
 import styles  from '../styles/Services.module.css';
 const Services = () => {
