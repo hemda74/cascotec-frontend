37679b08c215a21303b00281260849ef23851e8d
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-19 22:50:58 +0200

    change border color

--- app/components/VisualPresentations.tsx
+++ app/components/VisualPresentations.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styles from '../styles/EducationalBooklets.module.css'
+import styles from '../styles/VisualPresentations.module.css'
 import Link from 'next/link'
 import vp from '../public/imagess/visualpresentations.jpg'
 import Image from 'next/image'
-import { VscFilePdf } from 'react-icons/vsc'
+import { BsPlayFill } from 'react-icons/bs'
 const VisualPresentations= () => {
 return(
 <>
@@ -11,48 +11,120 @@ return(
       <h6 className="text-white fw-semibold mb-4" >{`Home > Educational Booklets`}</h6>
       <h2 className={`text-white fw-bold mb-5 `} >Educational Booklets</h2>
     </div>
-    <div className={` container`}>
-        <div className="row gy-5 gx-lg-5">
-          <div className={`col-lg-12 ${styles.contactCard}`}>
-              <form action="forms/contact.php" method="post" role="form" className={`  ${styles.searchCard}`}>
-                  <div className=" d-flex justify-content-between">
-                      <div className={`col-md-4 col-lg-4 col-sm-6 col-xs-6  form-group  ${styles.formGroup}`}>
+    <div className={` container  `}>
+        <div className="row gy-5 gx-lg-5 ">
+           <div className={`col-lg-10 m-auto row `}>
+                <form action="forms/contact.php" method="post" role="form" >
+                    <div className=" d-flex justify-content-between">
+                        <div className={`col-md-4 col-lg-4 col-sm-6 col-xs-6 form-group ${styles.formGroup}`}>
                           <p>3 Visual presentations</p>
-                      </div>
-                      <div className={`col-md-4 col-lg-2 col-sm-6 col-xs-6  form-group  ${styles.formGroup}`}>
+                        </div>
+                        <div className={`col-md-4 col-lg-2 col-sm-6 col-xs-6  form-group  ${styles.formGroup}`}>
                           <input type="text" name="name" className={`form-control ${styles.formControl}`} id="name" placeholder="Search..." required/>
-                      </div>
+                        </div>
                   </div>
               </form>
           </div>
-          <div className="row">
-          <div className={`col-xl-4 col-lg-6 col-md-6 col-sm-12 ${styles.cardContainer}`}>
-          <div className={`card p-3 ${styles.cardContainer}`}>
-                <Image src={vp} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <p className="card-text">{`Some quick example text to build on the card title and make up the bulk of the card's content.`}</p>
-                </div>
-                </div>
-        </div>
-        <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
-        <div className={`card p-3`}>
-                <Image src={vp} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                <p className="card-text">{`Some quick example text to build on the card title and make up the bulk of the card's content.`}</p>
-                </div>
-                </div>
-        </div>
-        <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
-        <div className={`card p-3`}>
-                <Image src={vp} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                <p className="card-text">{`Some quick example text to build on the card title and make up the bulk of the card's content.`}</p>
-                </div>
-                </div>
-            </div>
-        </div>
+          <div className={` container  `}>
+        <div className="row gy-5 gx-lg-5">
+          <div className={`col-lg-10 m-auto row `}>
+          <div className={`col-xl-4 col-lg-6 col-md-6 col-sm-12 ${styles.teamMember2}`}>
+          <div className={``}>
+                            <figure className={`${styles.teamMemberFigure} `}>
+                              <div className='p-2 '>
+                              <Image src={vp} layout='fill' alt="" className={`${styles.cardImage} p-3 `}/>
+                              </div>
+                              <figcaption className={`${styles.figCaption}  d-flex `}>
+                                <div className={` ${styles.accretiedCaption}`}>
+                                    <a href="https://www.youtube.com/embed/tv-8cJfQbqc" target="_blank" rel="noreferrer" >
+                                        <span className="text-white">
+                                            <BsPlayFill className='m-0' size={45}/></span>
+                                    </a>
+                                </div> 
+                              </figcaption>
+                            </figure>
+                            <span className={`${styles.textCard} fw-bold`}>Introducing the Gulf Accreditation Center from some members</span>
+                        </div>
+          </div>
+          <div className={`col-xl-4 col-lg-6 col-md-6 col-sm-12 ${styles.teamMember2}`}>
+          <div className={``}>
+                            <figure className={`${styles.teamMemberFigure} `}>
+                              <div className='p-2 '>
+                              <Image src={vp} layout='fill' alt="" className={`${styles.cardImage} p-3 `}/>
+                              </div>
+                              <figcaption className={`${styles.figCaption}  d-flex `}>
+                                <div className={` ${styles.accretiedCaption}`}>
+                                    <a href="https://www.youtube.com/embed/tv-8cJfQbqc" target="_blank" rel="noreferrer" >
+                                        <span className="text-white">
+                                            <BsPlayFill className='m-0' size={45}/></span>
+                                    </a>
+                                </div> 
+                              </figcaption>
+                            </figure>
+                            <span className={`${styles.textCard} fw-bold`}>Introducing the Gulf Accreditation Center from some members</span>
+                        </div>
+          </div>
+          <div className={`col-xl-4 col-lg-6 col-md-6 col-sm-12 ${styles.teamMember2}`}>
+          <div className={``}>
+                            <figure className={`${styles.teamMemberFigure} `}>
+                              <div className='p-2 '>
+                              <Image src={vp} layout='fill' alt="" className={`${styles.cardImage} p-3 `}/>
+                              </div>
+                              <figcaption className={`${styles.figCaption}  d-flex `}>
+                                <div className={` ${styles.accretiedCaption}`}>
+                                    <a href="https://www.youtube.com/embed/tv-8cJfQbqc" target="_blank" rel="noreferrer" >
+                                        <span className="text-white">
+                                            <BsPlayFill className='m-0' size={45}/></span>
+                                    </a>
+                                </div> 
+                              </figcaption>
+                            </figure>
+                            <span className={`${styles.textCard} fw-bold`}>Introducing the Gulf Accreditation Center from some members</span>
+                        </div>
+          </div>
+          <div className={`col-xl-4 col-lg-6 col-md-6 col-sm-12 ${styles.teamMember2}`}>
+          <div className={``}>
+                            <figure className={`${styles.teamMemberFigure} `}>
+                              <div className='p-2 '>
+                              <Image src={vp} layout='fill' alt="" className={`${styles.cardImage} p-3 `}/>
+                              </div>
+                              <figcaption className={`${styles.figCaption}  d-flex `}>
+                                <div className={` ${styles.accretiedCaption}`}>
+                                    <a href="https://www.youtube.com/embed/tv-8cJfQbqc" target="_blank" rel="noreferrer" >
+                                        <span className="text-white">
+                                            <BsPlayFill className='m-0' size={45}/></span>
+                                    </a>
+                                </div> 
+                              </figcaption>
+                            </figure>
+                            <span className={`${styles.textCard} fw-bold`}>Introducing the Gulf Accreditation Center from some members</span>
+                        </div>
+          </div>
+          <div className={`col-xl-4 col-lg-6 col-md-6 col-sm-12 ${styles.teamMember2}`}>
+          <div className={`m-0 p-0`}>
+                            <figure className={`${styles.teamMemberFigure} `}>
+                              <div className='p-2 '>
+                              <Image src={vp} layout='fill' alt="" className={`${styles.cardImage} p-3 `}/>
+                              </div>
+                              <figcaption className={`${styles.figCaption}  d-flex `}>
+                                <div className={` ${styles.accretiedCaption}`}>
+                                    <a href="https://www.youtube.com/embed/tv-8cJfQbqc" target="_blank" rel="noreferrer" >
+                                        <span className="text-white">
+                                            <BsPlayFill className='m-0' size={45}/></span>
+                                    </a>
+                                </div> 
+                              </figcaption>
+                            </figure>
+                            <span className={`${styles.textCard} fw-bold`}>Introducing the Gulf Accreditation Center from some members</span>
+                        </div>
+          </div>    
+                     
         </div>
     </div>
+    </div>
+    </div>
+    </div>
+    
 </>
 )
 }
