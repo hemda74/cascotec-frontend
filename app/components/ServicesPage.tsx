40ce95ca04d8f3c543cd65f55e988ff2fa05230d
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-21 04:55:28 +0200

    update services page

--- app/components/ServicesPage.tsx
+++ app/components/ServicesPage.tsx
@@ -10,57 +10,32 @@ const ServicesPage = () => {
         <h6 className="text-white fw-semibold mb-4">{`Home > Services`}</h6>
         <h2 className={`text-white fw-bold mb-5 `}>Services</h2>
     </div>
-    <div className={`row gy-5  container justify-content-center align-items-center m-auto ${styles.formConatiner}`}>
-    <div className="card_accreditation d-flex ">
-                                    <div className={` mt-5 ${styles.accreditationThumbnail2div}`}>
-                                        <Image className={`${styles.accreditationThumbnail2}`}src={servicesimage} width={350} height={350} alt=""/>
-                                    </div>
-                                    <div className={`${styles.accreditationThumbnail}`}>
-                                        <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
-                                        <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017
-                                                  General requirements for the competence of testing and calibration laboratories. </p>
-                                          <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
-                                                                            </div>
-                                </div>
-                                <div className="card_accreditation d-flex ">    
-                                    <div className={`${styles.accreditationThumbnail} `}>
-                                        <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
-                                        <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017
-                                                  General requirements for the competence of testing and calibration laboratories. </p>
-                                          <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
-                                                                            </div>
-                                                                            <div className={` mt-5 ${styles.accreditationThumbnail2div}`}>
-                                        <Image className={`${styles.accreditationThumbnail2}`}src={servicesimage} width={350} height={350} alt=""/>
-                                    </div>
-                                </div>
-                                <div className="card_accreditation d-flex ">
-                                <div className={` mt-5 ${styles.accreditationThumbnail2div}`}>
-                                        <Image className={`${styles.accreditationThumbnail2}`}src={servicesimage} width={350} height={350} alt=""/>
-                                    </div>
-                                    <div className={`${styles.accreditationThumbnail}`}>
-                                        <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
-                                        <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017
-                                                  General requirements for the competence of testing and calibration laboratories. </p>
-                                          <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
-                                                                            </div>
-                                </div>
-                                <div className="card_accreditation d-flex ">    
-                                    <div className={`${styles.accreditationThumbnail}`}>
-                                        <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
-                                        <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017
-                                                  General requirements for the competence of testing and calibration laboratories. </p>
-                                          <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
-                                                                            </div>
-                                                                            <div className={` mt-5 ${styles.accreditationThumbnail2div}`}>
-                                        <Image className={`${styles.accreditationThumbnail2}`}src={servicesimage} width={350} height={350} alt=""/>
-                                    </div>
-                                </div>
-                               
-                              
-                                   
-                                  
-                                
-
+    <div className="container col-xl-8 col-lg-10 col-md-12 mb-5">
+        <span className={`fw-bold`}>SERVICES</span>
+        <p className={`fs-3 fw-bold ${styles.subtitleWord}`}>Accreditation made simple for you</p>
+        <span className='fw-semibold'>The services we offer foster mutual confidence between service providers and consumers.</span>
+    </div>
+    <div className="container col-xl-7 col-lg-8 col-md-12 col-sm-12 col-xs-12 mb-5">
+        <div className={`${styles.cardccreditation} d-flex mt-5 mb-5`}>
+            <div className={` mt-5  ${styles.accreditationThumbnail2div}`}>
+                <Image className={`${styles.accreditationThumbnailImage}`}src={servicesimage} width={450} height={450} alt=""/>
+            </div>
+            <div className={`${styles.accreditationThumbnailFirst}`}>
+                <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
+                <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017 General requirements for the competence of testing and calibration laboratories. </p>
+                <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
+            </div>
+        </div>
+        <div className={`${styles.cardccreditation2} d-flex mt-5 mb-5`}>
+            <div className={`${styles.accreditationThumbnailSecond} `}>
+                <span className='fs-4  fw-bold '>Accreditation of Testing Laboratories</span>
+                <p className='mb-5 mt-4'>GCC Accreditation Center provides accreditation services in the scheme of Testing Laboratories according to ISO/IEC 17025:2017 General requirements for the competence of testing and calibration laboratories. </p>
+                <Link href="/login" target="_blank" className='mt-2'><span className={`${styles.btnContainer}`}>Apply now</span></Link>
+            </div>
+            <div className={` mt-5 ${styles.accreditationThumbnail2div}`}>
+                <Image className={`${styles.accreditationThumbnailImage}`}src={servicesimage} width={400} height={400} alt=""/>
+            </div>
+        </div>
    </div>
 </>
     )
