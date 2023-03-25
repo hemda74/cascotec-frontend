a46101d2fee4b7d8d7fcd2ec811367a2705529a5
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-03-26 01:14:46 +0200

    update apply accr section

--- app/components/ApplyAcc.tsx
+++ app/components/ApplyAcc.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/ApplyAcc.module.css'
     <>
     <div className={`${styles.mainContanier}`}>
         <span className='fw-bold fs-1 text-white '>Ready to be accredited?</span>
-        <span className='fs-4 fw-semibold text-white d-block'>GCC Accreditation Center provides accreditation services in wide range of management schemes</span>
+        <span className='fs-4 fw-semibold text-white text-center '>GCC Accreditation Center provides accreditation services in wide range of management schemes</span>
         <button className={`  mt-4 rounded ${styles.SearchButton}`} type="submit"><span className='fs-6 fw-bold'>APPLY FOR ACCREDITATION NOW</span></button>
 
     </div>
