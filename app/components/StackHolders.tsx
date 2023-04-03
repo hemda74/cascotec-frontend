523443014eb36637f59fe5ca2cb5b9d584425ac6
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-04 01:43:27 +0200

    update stack holder page in small screens

--- app/components/StackHolders.tsx
+++ app/components/StackHolders.tsx
@@ -14,27 +14,27 @@ const StackHolders = () => {
                 <p className='fs-1' data-trans="Responsibilitiesofthecommittee">Responsibilities of the committee</p>
                 <p className="fs-5" data-trans="Responsibilitiesofthecommitteeb1">The committee is in charge of the implementation of the following tasks and functions</p>
             </div>
-            <div className=" row mt-5 ">
+            <div className=" row mt-5  ">
                 <div className="col-lg-2"></div>
-                <div className="col-lg-2 col-md-6 col-sm-12 col-xs-12">
+                <div className={` ${styles.innerContainer} col-lg-2 col-md-6 col-sm-12 col-xs-12`}>
                     <div className={`${styles.biggerCircle}`}>
                         <span className={` ${styles.numbersCircle}`}>1</span>
                     </div>
                     <small className='pt-3' data-trans="Examininganddiscussingsubjects">{`Examining and discussing subjects related to the accreditation activities of the CASCO Group and providing relevant recommendations.`}</small>
                 </div>
-                <div className="col-lg-2 col-md-6 col-sm-12 col-xs-12">
+                <div className={` ${styles.innerContainer} col-lg-2 col-md-6 col-sm-12 col-xs-12`}>
                     <div className={`${styles.biggerCircle}`}>
                     <span className={` ${styles.numbersCircle}`}>2</span>
                     </div>
                     <small className='pt-3'data-trans="Assessingthepolicies">{`Assessing the policies and strategies of the CASCO Group and offering opinions and recommendations to the General Director.`}</small>
                 </div>
-                <div className="col-lg-2 col-md-6 col-sm-12 col-xs-12 d-block">
+                <div className={` ${styles.innerContainer} col-lg-2 col-md-6 col-sm-12 col-xs-12`}>
                     <div className={`${styles.biggerCircle}`}>
                     <span className={` ${styles.numbersCircle}`}>3</span>
                     </div>
                     <small className='pt-3' data-trans="Evaluatingtheplans">{`Evaluating the plans and programs proposed by the General Director of the CASCO Group and making recommendations accordingly.`}</small>
                 </div>
-                <div className="col-lg-2 col-md-6 col-sm-12 col-xs-12 d-block me-2 ms-0">
+                <div className={` ${styles.innerContainer} col-lg-2 col-md-6 col-sm-12 col-xs-12`}>
                     <div className={`${styles.itemOne}`}>
                         <div className={`${styles.biggerCircle}`}>
                         <span className={` ${styles.numbersCircle}`}>4</span>
@@ -54,7 +54,7 @@ const StackHolders = () => {
                     </div>
                 <div className="row col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 ms-2 text-lg-center mt-5">
                     <div className={`d-flex align-items-start ${styles.listContainer}`}>
-                        <div className={`nav flex-column nav-pills me-lg-4 ms-lg-4 ${styles.listinnerContainer1}`} id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <div className={`nav flex-column nav-pills me-lg-4 col-sm-1 ms-lg-4 ${styles.listinnerContainer1}`} id="v-pills-tab" role="tablist" aria-orientation="vertical">
                             <button className={` pb-2  ${styles.navLink} text-start fs-6`} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true" data-trans="Governmentauthorities">Government authorities</button>
                             <button className={` pb-2 ${styles.navLink} text-start fs-6`} id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false" data-trans="ConformityAssessmentBodies">Conformity Assessment Bodies</button>
                             <button className={` pb-2 ${styles.navLink} active text-start fs-6`} id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false" data-trans="GCCorganizationsIndustryandconsumers">{`GCC organizations, Industry and consumers`}</button>
