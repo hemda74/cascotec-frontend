720440b631e48abd528a36056be245b3a877a25c
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-05 00:41:57 +0200

    update rights and duties design

--- app/components/RightsAndDuties.tsx
+++ app/components/RightsAndDuties.tsx
@@ -4,29 +4,32 @@ import styles from '../styles/RightsAndDuties.module.css'
 import Image from 'next/image'
 import dutes from '../public/imagess/duties.png'
 import { GiCheckMark } from 'react-icons/gi'
-import { dir } from 'console'
+import Link from 'next/link'
 
 const RightsAndDuties = () => {
   return (
     <>
      <section className={` `}>
         <div className={`text-center ${styles.titleWord}`}>
-            <h6 className="text-white fw-semibold mb-4" data-trans="routeCASCORightsAndDuties">{`Home > About CASCO > Rights And Duties`}</h6>
+        <div className={`container mb-4`}>
+                <Link href={"/"}><a className="text-white fw-semibold mb-4">{`Home`}</a></Link>
+                <Link href={"/about"}><a className="text-white fw-semibold mb-4">{` > About CASCO`}</a></Link>
+                <Link href={"/rights-and-duties"}><a className="text-white fw-semibold mb-4" >{` > Rights And Duties`}</a></Link>
+            </div>
             <h2 className={`text-white fw-bold  `} data-trans="CASCORightsAndDuties">{`CASCO’s Rights And Duties`}</h2>
         </div>
-        <div className="container">
-            <div className="row mx-0 d-flex justify-content-around align-items-center">
-                <div className={`card-body col-xl-3 col-lg-3 col-md-6 col-sm-12`}>
-                    <span className={`me-lg-5 ${styles.spanHead}`} data-trans="CASCOSRIGHTSANDDUTIES">{`CASCO'S RIGHTS AND DUTIES`}</span>
-                    <h4 className='fw-bold me-lg-5 mt-2' data-trans="CASCOSRights">{`CASCO'S Rights`}</h4>
+        <div className="container col-lg-9 col-xl-9 col-md-12 col-sm-12 col-xs-12">
+            <div className="row mx-0 d-flex justify-content-center align-items-center ">
+                
+       
+            <div className="row   d-flex justify-content-around align-items-center">
+            <div className={`card-body mb-4 `}>
+                    <span className={`${styles.spanHead}`} data-trans="CASCOSRIGHTSANDDUTIES">{`CASCO'S RIGHTS AND DUTIES`}</span>
+                    <h4 className='fw-bold  mt-2' data-trans="CASCOSRights">{`CASCO'S Rights`}</h4>
                 </div>
-            </div>
-        </div>
-        <div className="container">
-            <div className="row mx-0  d-flex justify-content-around align-items-center">
-                <div className={`card-body col-xl-8 col-lg-8 col-md-12 col-sm-12 p-4`}>
+                <div className={`card-body`}>
                     <ul className={`${styles.ulClass}`}>
-                        <li className={`${styles.itemCheckTage} d-flex`}>
+                        <li className={`${styles.itemCheckTage} col-sm-12 d-flex`}>
                             <p><FcCheckmark size={20} className=''/></p>
                             <span  className='me-2 ms-2' data-trans='ritghs1'>{`GAC has the right to decline, withdraw, suspend or even reduce scope of its accredited CAB which doesn’t comply with the requirements of the accreditation criteria including applicable laws/regulation, or when it is found have falsified information or documents and when there’s clear evidence of its fraudulent behavior.`}</span>
                         </li>
@@ -34,7 +37,7 @@ const RightsAndDuties = () => {
                             <p><FcCheckmark size={20} className=''/></p>
                             <span  className='me-2 ms-2' data-trans='ritghs2'>{`To not accept the application from CAB that doesn’t provide required information & documents, and doesn’t cooperate to a level necessary to meet the timelines, or when found to be scam or fraudulent.`}</span>
                         </li>
-                        <li className={`${styles.itemCheckTage} d-flex `}>
+                        <li className={`${styles.itemCheckTage} d-flex mt-4`}>
                             <p><FcCheckmark size={20} className=''/></p>
                             <span  className='me-2 ms-2' data-trans='ritghs3'>{`To change type of assessment, frequency of assessment and conduct unannounced assessment as result of previous assessment outcome, due to complaint or appeals or sourced information which impacts the image of GAC or affects integrity and competence of the CAB.`}</span>
                         </li>
@@ -43,6 +46,7 @@ const RightsAndDuties = () => {
                             <span data-trans='ritghs4' className='me-2 ms-2'>{`To assess the management system of the CAB and require any document and access to location and facilities in relation to the scope of accreditation during the accreditation cycle of the CAB.`}</span>
                         </li>
                     </ul>          
+                </div>
                 </div>
             </div>
         </div>
