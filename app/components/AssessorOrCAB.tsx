86dbe8523f7fb3571b714d8d7912b38932758713
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-04 06:28:03 +0200

    update assessor or cab section

--- app/components/AssessorOrCAB.tsx
+++ app/components/AssessorOrCAB.tsx
@@ -10,16 +10,16 @@ const AssessorOrCAB = () => {
                 <div className={`${styles.innerAssessorCab}`}>
                     <h4 className={`${styles.headAssessorCab}`}>Assessor or CAB?</h4>
                 </div>
-                <div className={`${styles.bodyAssessorCab}  `}>
+                <div className={`${styles.bodyAssessorCab} `}>
                     <div className={`container ${styles.innerContainer} `}>
                         <div className={`row justify-content-center  ${styles.mainDivContainer}`}>
                              <div className={`col-md-6 col-sm-12 col-xl-6 col-xs-12 col-lg-6 ${styles.cardCertificate} me-lg-4 ms-lg-4 `}>
                                             <img src="/images/home_page/assessor-illustration.png" alt=""/>
-                                            <button className={`myInfo p-2 rounded ${styles.btnClass} `} type="submit">Send Message</button>
+                                            <button className={`myInfo  rounded ${styles.btnClassAssessor} `}>{`ASSESSOR WORKSPACE`}</button>
                                     </div>
                                         <div className={` col-md-6 col-sm-12 col-xs-12 col-xl-6 col-lg-6 ${styles.cardCertificate2}`}>
                                         <img src="/images/home_page/cab-illustration.png" alt=""/>
-                                            <button className={`myInfo p-2 rounded ${styles.btnClass} `} type="submit">Send Message</button>
+                                            <button className={`myInfo  rounded ${styles.btnClassCab} `}>{`CAB WORKSPACE`}</button>
                                         </div>
                              </div>
                     </div>
