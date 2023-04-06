d89777c06759b6d405b7cfba076fe5042de14786
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-06 06:58:59 +0200

    update hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,16 +4,47 @@ import styles from'../styles/Hero.module.css'
 const Hero = () => {
   return (
 <>
-<div className={`d-flex align-items-center justify-content-center ${styles.landHead}`}>
-      <div className={`container ${styles.landCont}`}>
-            <span className={`fs-1 text-center fw-bold ${styles.landQuet1}`} data-trans="CompanyName">
-                Welcome to CASCO 
-            </span>
-            <span className={`fs-2  mt-4 fw-semibold ${styles.landQuet2}`}data-trans="CompanyNamesubtitle">
-            For Accreditation Certificates
-            </span>
-      </div>
+<div id="carouselExampleDark" className="carousel carousel-dark  slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="5000">
+      <img src="/images/home_page/hero/hero_1.jpg" className="d-block w-100" alt="..."/>
+      <div className={`carousel-caption text-white d-md-block `}>
+        <h1>Services</h1>
+        <p>Some representative placeholder content for the first slide.</p>
+        <button className={` myInfo rounded ${styles.SearchButton}`} type="submit">Go To Services</button>
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="5000">
+      <img src="/images/home_page/hero/hero_2.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption text-white d-md-block">
+        <h1>Courses</h1>
+        <p>Some representative placeholder content for the second slide.</p>
+        <button className={` myInfo rounded ${styles.SearchButton}`} type="submit">Go To Courses</button>
+      </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="5000">
+      <img src="/images/home_page/hero/hero_3.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption text-white d-md-block">
+        <h1>Accredited Bodies</h1>
+        <p>Some representative placeholder content for the third slide.</p>
+        <button className={` myInfo d-inline rounded ${styles.SearchButton}`} type="submit">Go To Accredited Bodies</button>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev " type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
 </>  
 )
 }
