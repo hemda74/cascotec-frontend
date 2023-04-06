59d658ac3452e4dc748be717dc10c097a600df9a
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-06 06:58:24 +0200

    add 404 page

---
+++ app/pages/404.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+import FourOhFourImage from '../public/404.svg'
+
+type Props = {};
+
+// a customized 404 page.
+const FourOhFour: NextPage = (props: Props) => {
+  return (
+    <>
+      <div className="container d-flex align-items-center justify-content-center ">
+        <div className="vh-90 align-items-center justify-content-center">
+          <div className="">
+            <div className="d-flex flex-column justify-content-center align-items-center w-80">
+              <h1 className="fw-bold " data-trans="fourOhfourNotFound">
+                Page Not Found
+              </h1>
+              <h3 className="myPrimary-Text" data-trans="fourOhfourMsg">
+                 Looks Like this page doesn't Exist.
+              </h3>
+            </div>
+          </div>
+          <div className=" d-flex align-items-center justify-content-center ">
+            <Image
+              alt="FourOhFourShape"
+              src={FourOhFourImage}
+              width={350}
+              height={350}
+            />
+          </div>
+        </div>
+        <style jsx>{`
+        .container {
+          margin-top: 15rem;
+        }
+        
+      `}</style>
+      </div>
+    </>
+  );
+};
+
+export default FourOhFour;
