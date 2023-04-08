cf25d02e45d4f6c49b999621c29071116081c7d3
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-08 08:13:00 +0200

    make document sectionDynamic

--- app/components/DocumentsModal.tsx
+++ app/components/DocumentsModal.tsx
@@ -14,11 +14,14 @@ const DocumentsModal = (props: Props) => {
       <div
         className={`modal-dialog modal-lg modal-dialog-centered ${styles.modalWidth}`}>
         <div className="modal-content">
+        <div className="modal-header">
+        <p className="modal-title fs-5 me-2 ms-2 fw-semibold">Changelog</p>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
           <div className="modal-body p-0">
             <div className="d-flex flex-column align-items-start col-sm-12">
-              <p className="fs-5 me-2 ms-2 fw-semibold">Changelog</p>
-              <div className='p-4 bg-light'>
-              <small className='me-2 ms-2'>Minor updates to document the current practices: Added about selection and appointment of the ADC & FMs, reference to AC 4.2 added for such records. Rejection of assessors/experts when objected by the client of the CAB. References to IAF MD documents added</small>
+              <div className='p-4 '>
+              <small className=''>Minor updates to document the current practices: Added about selection and appointment of the ADC & FMs, reference to AC 4.2 added for such records. Rejection of assessors/experts when objected by the client of the CAB. References to IAF MD documents added</small>
               </div>
             </div>
           </div>
--- app/pages/documents.tsx
+++ app/pages/documents.tsx
@@ -5,6 +5,70 @@ import { NextPageWithLayout } from './_app';
 import Documents from '../components/Documents';
 type Props = {};
 // dymmy data for ui till handle working with api
+const dummyData = {
+  ducoments: [
+    {
+      document_name:'AC 2.0 Applications For Accreditation',
+      document_pdf:'../HPC 0069.pdf', 
+      document_id: 0,
+      document_version:'7.2',
+      document_issued_date:'02 Feb 2023',
+      document_category:'Internal Document',
+      document_sub_category:'Procedures',
+      document_accrediation_type:'Calibration Lab,Halal Product Certification ,Inspection Body ,Management System Certification ,Medical Lab ,Personnel Certification Body ,Product Certification ,Prof Testing Provider ,Reference Material Producer,Testing Lab',
+      document_changelog:'Minor update in relation to internal audit finding 2021 to add a sentence that the CAB is "(normally those Conformity Assessment Bodies covered by IAF MLA)"',
+      document_for_Code:'flush-collapseOne'
+    },
+    {
+      document_name:'AC 2.1 Application form and Accreditation Agreement',
+      document_pdf:'../HPC 0069.pdf', 
+      document_id: 1,
+      document_version:'5.2',
+      document_issued_date:'02 Feb 2023',
+      document_category:'Internal Document',
+      document_sub_category:'Procedures',
+      document_accrediation_type:'Calibration Lab,Halal Product Certification ,Inspection Body ,Management System Certification ,Medical Lab ,Personnel Certification Body ,Product Certification ,Prof Testing Provider ,Reference Material Producer,Testing Lab',
+      document_changelog:'Minor update in relation to internal audit finding 2021 to add a sentence that the CAB is "(normally those Conformity Assessment Bodies covered by IAF MLA)"',
+      document_for_Code:'panelsStayOpen-collapseTwo'
+    },
+    {
+      document_name:'EXT 6.0 ILAC-R7 : Rules For The Use Of The ILAC MRA Mark',
+      document_pdf:'../HPC 0069.pdf', 
+      document_id: 2,
+      document_version:'3.2',
+      document_issued_date:'02 Feb 2023',
+      document_category:'Internal Document',
+      document_sub_category:'Procedures',
+      document_accrediation_type:'Calibration Lab,Halal Product Certification ,Inspection Body ,Management System Certification ,Medical Lab ,Personnel Certification Body ,Product Certification ,Prof Testing Provider ,Reference Material Producer,Testing Lab',
+      document_changelog:'Minor update in relation to internal audit finding 2021 to add a sentence that the CAB is "(normally those Conformity Assessment Bodies covered by IAF MLA)"',
+      document_for_Code:'panelsStayOpen-collapseThree'
+    },
+    {
+      document_name:'AC 4.0 Document Review',
+      document_pdf:'../HPC 0069.pdf', 
+      document_id: 3,
+      document_version:'8.1',
+      document_issued_date:'02 Feb 2023',
+      document_category:'Internal Document',
+      document_sub_category:'Procedures',
+      document_accrediation_type:'Calibration Lab,Halal Product Certification ,Inspection Body ,Management System Certification ,Medical Lab ,Personnel Certification Body ,Product Certification ,Prof Testing Provider ,Reference Material Producer,Testing Lab',
+      document_changelog:'Minor update in relation to internal audit finding 2021 to add a sentence that the CAB is "(normally those Conformity Assessment Bodies covered by IAF MLA)"',
+      document_for_Code:'panelsStayOpen-collapseFour'
+    },
+    {
+      document_name:'AC 5.0 Assessment preparation and team selection',
+      document_pdf:'../HPC 0069.pdf', 
+      document_id: 4,
+      document_version:'1.6',
+      document_issued_date:'02 Feb 2023',
+      document_category:'Internal Document',
+      document_sub_category:'Procedures',
+      document_accrediation_type:'Calibration Lab,Halal Product Certification ,Inspection Body ,Management System Certification ,Medical Lab ,Personnel Certification Body ,Product Certification ,Prof Testing Provider ,Reference Material Producer,Testing Lab',
+      document_changelog:'Minor update in relation to internal audit finding 2021 to add a sentence that the CAB is "(normally those Conformity Assessment Bodies covered by IAF MLA)"',
+      document_for_Code:'panelsStayOpen-collapseFive'
+    },
+  ],
+};
 const services:NextPageWithLayout = (props:Props) => {
   return (
     <>
@@ -12,7 +76,7 @@ const services:NextPageWithLayout = (props:Props) => {
         <title>Documents | CASCO</title>
     </Head>
     <main>
-      <Documents/>
+      <Documents ducoments={dummyData.ducoments} />
     </main>
     </>
   )
@@ -21,3 +85,5 @@ const services:NextPageWithLayout = (props:Props) => {
 services.getLayout = function getLayout(contactus: ReactElement) {
 return <ViewerLayout childern={contactus}></ViewerLayout>;}
 export default services;
+
+
