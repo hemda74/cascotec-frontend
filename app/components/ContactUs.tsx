2d8455be57f0a6eb6a3b3975c105203010893890
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-10 06:31:51 +0200

    update contact information

--- app/components/ContactUs.tsx
+++ app/components/ContactUs.tsx
@@ -21,81 +21,81 @@ const ContactUs = () => {
             <div className="col-lg-4 col-md-6 p-3 " >
             
             <div className={`  ${styles.firstIcons}`}>
-            <p className={`fs-4 mb-4 ${styles.postTitle}`}>Dubai Branch</p>
+            <p className={`fs-4 mb-4 ${styles.postTitle}`}>Egypt Branch</p>
                         <p className={`${styles.iconsColor}`}>
                         <BsFillTelephoneFill  size={33}/>
                         </p>
-                        <p className=''>+20 112 1900 466</p>
+                        <p className=''>+20 23963913</p>
                         <p className={`${styles.iconsColor}`}>
                         <FiMail  size={30}/>
                         </p>
                         <p className='mt-0 me-2'>info@casco.com</p>
                     <div className={`  `}>
-                    <div className=' col-sm-12'>
+                    {/* <div className=' col-sm-12'>
                     <p className={`${styles.iconsColor}`}>
                         <FaFax  size={30}/>
                         </p>
                         <p className=''>+20 112 1900 466</p>
-                        </div>
+                        </div> */}
                         <div className=' col-sm-12'>
                         <p className={`${styles.iconsColor2}`}>
                         <FaMapMarkerAlt  size={35} />
                         </p>
-                        <p className=''>Kingdom of Saudia Arabia P.O.Box 85245 Riyadh 11691</p>
+                        <p className=''>61 Al Falki, Bab Al Louq, Abdeen, Cairo Governorate 11513, Egypt</p>
                         </div>
                     </div>   
                     </div>          
             </div>
             <div className="col-lg-4 col-md-6 p-3 " > 
             <div className={`  ${styles.firstIcons}`}>
-            <p className={`fs-4 mb-4 ${styles.postTitle}`}>KSA Branch</p>
+            <p className={`fs-4 mb-4 ${styles.postTitle}`}>UAE Branch</p>
                         <p className={`${styles.iconsColor}`}>
                         <BsFillTelephoneFill  size={33}/>
                         </p>
-                        <p className=''>+20 112 1900 466</p>
+                        <p className=''>+971 52 526 1012</p>
                         <p className={`${styles.iconsColor}`}>
                         <FiMail  size={30}/>
                         </p>
-                        <p className='mt-0 me-2'>info@casco.com</p>
+                        <p className='mt-0 me-2'>dubai@cascotec.com</p>
                     <div className={`  `}>
-                    <div className=' col-sm-12'>
-                    <p className={`${styles.iconsColor}`}>
+                    {/*<div className=' col-sm-12'>
+                     <p className={`${styles.iconsColor}`}>
                         <FaFax  size={30}/>
                         </p>
                         <p className=''>+20 112 1900 466</p>
-                        </div>
+                        </div> */}
                         <div className=' col-sm-12'>
                         <p className={`${styles.iconsColor2}`}>
                         <FaMapMarkerAlt  size={35} />
                         </p>
-                        <p className=''>Kingdom of Saudia Arabia P.O.Box 85245 Riyadh 11691</p>
+                        <p className=''>Floor #14 flat #1401 Victoria Building 31 new al taawun road Sharjah - UAE. land Mark Oriana Hospital</p>
                         </div>
                     </div>   
                     </div>          
             </div>
             <div className="col-lg-4 col-md-6 p-3 " >
             <div className={`  ${styles.firstIcons}`}>
-            <p className={`fs-4 mb-4 ${styles.postTitle}`}>Egypt Branch</p>
+            <p className={`fs-4 mb-4 ${styles.postTitle}`}>KSA Branch</p>
                         <p className={`${styles.iconsColor}`}>
                         <BsFillTelephoneFill  size={33}/>
                         </p>
-                        <p className=''>+20 112 1900 466</p>
+                        <p className=''>+966 50 195 1017</p>
                         <p className={`${styles.iconsColor}`}>
                         <FiMail  size={30}/>
                         </p>
-                        <p className='mt-0 me-2'>info@casco.com</p>
+                        <p className='mt-0 me-2'>ksa@cascotec.com</p>
                     <div className={`  `}>
-                    <div className=' col-sm-12'>
+                    {/* <div className=' col-sm-12'>
                     <p className={`${styles.iconsColor}`}>
                         <FaFax  size={30}/>
                         </p>
-                        <p className=''>+20 112 1900 466</p>
-                        </div>
+                        <p className=''>+966 50 195 1017</p>
+                        </div> */}
                         <div className=' col-sm-12'>
                         <p className={`${styles.iconsColor2}`}>
                         <FaMapMarkerAlt  size={35} />
                         </p>
-                        <p className=''>Kingdom of Saudia Arabia P.O.Box 85245 Riyadh 11691</p>
+                        <p className=''>KSA - Riyadh - Mosa Bin Nasser St. Borj Alnamer - office No: 506</p>
                         </div>
                     </div>   
                     </div>          
@@ -106,25 +106,24 @@ const ContactUs = () => {
         <div className="row">
             <div className="col-lg-4 col-md-6 p-3" >
       <div className={`${styles.mapContact}`}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3625.623773892965!2d46.62362161499904!3d24.671075184143564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f1d573927541d%3A0xb9bf38666935b508!2sGCC%20Accreditation%20Center%20-%20GAC!5e0!3m2!1sen!2seg!4v1612347619354!5m2!1sen!2seg" frameBorder={0} allowFullScreen></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.682897233059!2d31.2425979202108!3d30.04595398197049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458415e7080b205%3A0x60cf5d8571c8083b!2zQ0FTQ08gRm9yIEFzc2Vzc21lbnQgU2VydmljZXMgTC5MLkMuINmD2KfYs9mD2Ygg2YTYrtiv2YXYp9iqINin2YTYqtmC2YrZitmF!5e0!3m2!1sar!2seg!4v1681100283875!5m2!1sar!2seg"   frameBorder={0} ></iframe>
       </div>
       </div>
       <div className="col-lg-4 col-md-6 p-3" >
       <div className={`${styles.mapContact}`}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3625.623773892965!2d46.62362161499904!3d24.671075184143564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f1d573927541d%3A0xb9bf38666935b508!2sGCC%20Accreditation%20Center%20-%20GAC!5e0!3m2!1sen!2seg!4v1612347619354!5m2!1sen!2seg" frameBorder={0} allowFullScreen></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1377.5245954970412!2d55.37754646020215!3d25.3141902703356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5b8c60f62301%3A0xe89411747b8d1984!2sVictoria%20building!5e0!3m2!1sen!2ssa!4v1681100863947!5m2!1sen!2ssa"></iframe>
       </div>
       </div>
       <div className="col-lg-4 col-md-6 p-3" >
       <div className={`${styles.mapContact}`}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3625.623773892965!2d46.62362161499904!3d24.671075184143564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f1d573927541d%3A0xb9bf38666935b508!2sGCC%20Accreditation%20Center%20-%20GAC!5e0!3m2!1sen!2seg!4v1612347619354!5m2!1sen!2seg" frameBorder={0} allowFullScreen></iframe>
-      </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3624.6851579025974!2d46.6797129!3d24.703347500000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03aac9be175d%3A0x785d35fc020f008!2zbXV0aG1lciBjb25zdWx0YW50INmF2KvZhdixINmE2YTYp9iz2KrYtNin2LHYp9iq!5e0!3m2!1sen!2ssa!4v1681100567164!5m2!1sen!2ssa" ></iframe>      </div>
       </div>
       </div>
       </div>
       <div className={` container ${styles.formConatiner}`}>
         <div className="row gy-5 gx-lg-5">
           <div className={`col-lg-12 ${styles.contactCard}`}>
-            <form action="forms/contact.php" method="post" role="form" className={` php-email-form ${styles.phpEmailForm}`}>
+            <form action="" method="post" role="form" className={` ${styles.phpEmailForm}`}>
               <div className="row">
               <p>Please fill out the form below, and one of our representatives will contact you within 24 hours.</p>
                 <div className={`col-md-6 form-group mt-4 ${styles.formGroup}`}>
@@ -134,7 +133,7 @@ const ContactUs = () => {
                   <input type="email" className={`form-control ${styles.formControl}`} name="email" id="email" placeholder="Your Email" required/>
                 </div>
                 <div className={`col-md-6 form-group mt-4 ${styles.formGroup}`}>
-                <select  className={`form-select ${styles.formControl}`}  id="site-holidayes-input" >
+                <select  className={`form-select ${styles.formControl}`}>
                       <option>Contact</option>
                       <option>Inquiry</option>
                       <option>Complaints & Seggestions</option>
@@ -410,7 +409,7 @@ const ContactUs = () => {
                 <div className={`error-message ${styles.errorMassage}`}></div>
                 <div className={`sent-message ${styles.sentMassage}`}>Your message has been sent. Thank you!</div>
               </div>
-              <div className="text-center"><button className={`myInfo p-2 rounded fs-5 ${styles.btnClass} `} type="submit">Send Message</button></div>
+              <div className="text-center"><button className={`myInfo p-2 rounded fs-6 ${styles.btnClass} `} type="submit">Send Message</button></div>
             </form>
           </div>
         </div>
