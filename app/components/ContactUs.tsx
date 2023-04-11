f57c1d8aa80324070e9c6e6836bb34ce57d923ea
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-12 00:20:22 +0200

    update site

--- app/components/ContactUs.tsx
+++ app/components/ContactUs.tsx
@@ -9,99 +9,98 @@ const ContactUs = () => {
   return (
    <>
      <section id="contact" className="contact">
-     <div className={`text-center ${styles.titleWord}`}>
-     <div className={`container mb-4`}>
-                    <Link href={"/"}><a className="text-white fw-semibold mb-4">{`Home`}</a></Link>
-                    <Link href={"/contact-us"}><a className="text-white fw-semibold mb-4">{` > Contact Us`}</a></Link>
-                </div>
-          <h2 className="text-white fw-bold mb-5" data-trans="ContactUs">Contact Us</h2>
-          </div>
-          <div className="container col-xl-10 col-lg-12 col-md-12 mb-5">
-        <div className="row">
-            <div className="col-lg-4 col-md-6 p-3 " >
-            
-            <div className={`  ${styles.firstIcons}`}>
-            <p className={`fs-4 mb-4 ${styles.postTitle}`}>Egypt Branch</p>
-                        <p className={`${styles.iconsColor}`}>
-                        <BsFillTelephoneFill  size={33}/>
-                        </p>
-                        <p className=''>+20 23963913</p>
-                        <p className={`${styles.iconsColor}`}>
-                        <FiMail  size={30}/>
-                        </p>
-                        <p className='mt-0 me-2'>info@casco.com</p>
-                    <div className={`  `}>
-                    {/* <div className=' col-sm-12'>
-                    <p className={`${styles.iconsColor}`}>
-                        <FaFax  size={30}/>
-                        </p>
-                        <p className=''>+20 112 1900 466</p>
-                        </div> */}
-                        <div className=' col-sm-12'>
-                        <p className={`${styles.iconsColor2}`}>
-                        <FaMapMarkerAlt  size={35} />
-                        </p>
-                        <p className=''>61 Al Falki, Bab Al Louq, Abdeen, Cairo Governorate 11513, Egypt</p>
-                        </div>
-                    </div>   
-                    </div>          
+        <div className={`text-center ${styles.titleWord}`}>
+            <div className={`container mb-4`}>
+                <Link href={"/"}><a className="text-white fw-semibold mb-4" data-trans="homepageurl">{`Home`}</a></Link>
+                <Link href={"/contact-us"}><a className="text-white fw-semibold mb-4" data-trans="ContactUsHeadLink">{` > Contact Us`}</a></Link>
             </div>
-            <div className="col-lg-4 col-md-6 p-3 " > 
-            <div className={`  ${styles.firstIcons}`}>
-            <p className={`fs-4 mb-4 ${styles.postTitle}`}>UAE Branch</p>
-                        <p className={`${styles.iconsColor}`}>
-                        <BsFillTelephoneFill  size={33}/>
-                        </p>
-                        <p className=''>+971 52 526 1012</p>
-                        <p className={`${styles.iconsColor}`}>
-                        <FiMail  size={30}/>
-                        </p>
-                        <p className='mt-0 me-2'>dubai@cascotec.com</p>
-                    <div className={`  `}>
-                    {/*<div className=' col-sm-12'>
-                     <p className={`${styles.iconsColor}`}>
-                        <FaFax  size={30}/>
-                        </p>
-                        <p className=''>+20 112 1900 466</p>
-                        </div> */}
-                        <div className=' col-sm-12'>
-                        <p className={`${styles.iconsColor2}`}>
-                        <FaMapMarkerAlt  size={35} />
-                        </p>
-                        <p className=''>Floor #14 flat #1401 Victoria Building 31 new al taawun road Sharjah - UAE. land Mark Oriana Hospital</p>
-                        </div>
-                    </div>   
-                    </div>          
-            </div>
-            <div className="col-lg-4 col-md-6 p-3 " >
-            <div className={`  ${styles.firstIcons}`}>
-            <p className={`fs-4 mb-4 ${styles.postTitle}`}>KSA Branch</p>
-                        <p className={`${styles.iconsColor}`}>
-                        <BsFillTelephoneFill  size={33}/>
-                        </p>
-                        <p className=''>+966 50 195 1017</p>
-                        <p className={`${styles.iconsColor}`}>
-                        <FiMail  size={30}/>
-                        </p>
-                        <p className='mt-0 me-2'>ksa@cascotec.com</p>
-                    <div className={`  `}>
-                    {/* <div className=' col-sm-12'>
-                    <p className={`${styles.iconsColor}`}>
-                        <FaFax  size={30}/>
-                        </p>
-                        <p className=''>+966 50 195 1017</p>
-                        </div> */}
-                        <div className=' col-sm-12'>
-                        <p className={`${styles.iconsColor2}`}>
-                        <FaMapMarkerAlt  size={35} />
-                        </p>
-                        <p className=''>KSA - Riyadh - Mosa Bin Nasser St. Borj Alnamer - office No: 506</p>
-                        </div>
-                    </div>   
-                    </div>          
-            </div>
+            <h2 className="text-white fw-bold mb-5" data-trans="ContactUs">Contact Us</h2>
         </div>
-      </div>
+        <div className="container col-xl-10 col-lg-12 col-md-12 mb-5">
+          <div className="row">
+              <div className="col-lg-4 col-md-6 p-3 " > 
+                <div className={`${styles.firstIcons}`}>
+                    <p className={`fs-4 mb-4 ${styles.postTitle}`}>Egypt Branch</p>
+                    <p className={`${styles.iconsColor}`}>
+                        <BsFillTelephoneFill  size={33}/>
+                    </p>
+                    <p className=''>+20 23963913</p>
+                    <p className={`${styles.iconsColor}`}>
+                        <FiMail  size={30}/>
+                    </p>
+                    <p className='mt-0 me-2'>info@casco.com</p>
+                    <div className={``}>
+                      {/* <div className=' col-sm-12'>
+                      <p className={`${styles.iconsColor}`}>
+                          <FaFax  size={30}/>
+                          </p>
+                          <p className=''>+20 112 1900 466</p>
+                          </div> */}
+                        <div className='col-sm-12'>
+                            <p className={`${styles.iconsColor2}`}>
+                            <FaMapMarkerAlt  size={35} />
+                            </p>
+                            <p className='' data-trans="egyptadress">61 Al Falki, Bab Al Louq, Abdeen, Cairo Governorate 11513, Egypt</p>
+                        </div>
+                    </div>   
+                </div>          
+              </div>
+              <div className="col-lg-4 col-md-6 p-3 " > 
+              <div className={`  ${styles.firstIcons}`}>
+              <p className={`fs-4 mb-4 ${styles.postTitle}`}>UAE Branch</p>
+                          <p className={`${styles.iconsColor}`}>
+                          <BsFillTelephoneFill  size={33}/>
+                          </p>
+                          <p className=''>+971 52 526 1012</p>
+                          <p className={`${styles.iconsColor}`}>
+                          <FiMail  size={30}/>
+                          </p>
+                          <p className='mt-0 me-2'>dubai@cascotec.com</p>
+                      <div className={`  `}>
+                      {/*<div className=' col-sm-12'>
+                      <p className={`${styles.iconsColor}`}>
+                          <FaFax  size={30}/>
+                          </p>
+                          <p className=''>+20 112 1900 466</p>
+                          </div> */}
+                          <div className=' col-sm-12'>
+                          <p className={`${styles.iconsColor2}`}>
+                          <FaMapMarkerAlt  size={35} />
+                          </p>
+                          <p className=''>Floor #14 flat #1401 Victoria Building 31 new al taawun road Sharjah - UAE. land Mark Oriana Hospital</p>
+                          </div>
+                      </div>   
+                      </div>          
+              </div>
+              <div className="col-lg-4 col-md-6 p-3 " >
+              <div className={`  ${styles.firstIcons}`}>
+              <p className={`fs-4 mb-4 ${styles.postTitle}`}>KSA Branch</p>
+                          <p className={`${styles.iconsColor}`}>
+                          <BsFillTelephoneFill  size={33}/>
+                          </p>
+                          <p className=''>+966 50 195 1017</p>
+                          <p className={`${styles.iconsColor}`}>
+                          <FiMail  size={30}/>
+                          </p>
+                          <p className='mt-0 me-2'>ksa@cascotec.com</p>
+                      <div className={`  `}>
+                      {/* <div className=' col-sm-12'>
+                      <p className={`${styles.iconsColor}`}>
+                          <FaFax  size={30}/>
+                          </p>
+                          <p className=''>+966 50 195 1017</p>
+                          </div> */}
+                          <div className=' col-sm-12'>
+                          <p className={`${styles.iconsColor2}`}>
+                          <FaMapMarkerAlt  size={35} />
+                          </p>
+                          <p className=''>KSA - Riyadh - Mosa Bin Nasser St. Borj Alnamer - office No: 506</p>
+                          </div>
+                      </div>   
+                      </div>          
+              </div>
+          </div>
+        </div>
       <div className="container col-xl-10 col-lg-12 col-md-12 mb-5">
         <div className="row">
             <div className="col-lg-4 col-md-6 p-3" >
@@ -414,7 +413,7 @@ const ContactUs = () => {
           </div>
         </div>
       </div>
-    </section>
+      </section>
    </>
   )
 }
