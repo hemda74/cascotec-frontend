53b281d0931a93ae9355a310ccca23a9e75db900
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-17 01:32:57 +0200

    create service link

---
+++ app/pages/services/[slug].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServiceCard from '../../components/ServiceCard'
+
+const slug = () => {
+  return (
+    <>
+    <ServiceCard/>
+    </>
+  )
+}
+
+export default slug
