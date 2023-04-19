470645517996ab5041fedf9d6d844e088d73d3bd
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-19 02:04:05 +0200

    add casco data

--- app/components/CoursesSectionHomePage.tsx
+++ app/components/CoursesSectionHomePage.tsx
@@ -17,9 +17,8 @@ const CoursesSectionHomePage = () => {
                             <div className="row mx-0">
                                 <div className="col-xl-4 col-lg-4 col-md-12 ">
                                     <div className="section_services_head pb-sm-5">
-                                        <span className={`${styles.spanFeature}`}>Courses</span>
-                                        <p  className="fw-bold fs-4 mt-2">Find your perfect program</p>
-                                        <p className={`${styles.cousesParageph}`}>The GCC was distinguished by providing courses, seminars, and training programs via online technology in line with the circumstances that may arise, and in keeping with the progress and development of information technology. In addition, we offer offline courses.</p>
+                                        <span className={`fw-bold fs-4 mt-2 ${styles.spanFeature}`} data-trans="COURSES">{`COURSES`}</span>
+                                        <p className={`${styles.cousesParageph}`} data-trans="cousesParageph">{`CASCO is known for its provision of courses, seminars, and training programs through online technology to adapt to any situation and keep up with advancements in information technology. Furthermore, we also offer in-person classroom courses.`}</p>
                                         <img src="/images/home_page/section_courses/section_courses.png" className="img_blog" alt="course_image" />
                                     </div>
                                 </div>
@@ -29,7 +28,7 @@ const CoursesSectionHomePage = () => {
                                     <Link href={'/courses/global-standard-for-food-safety-issue-9'}>
                                     <div className={`${styles.postBox} p-1`}>
                                         <div className={`${styles.postImage}`}><Image src={courseimage} className="img-fluid" alt="course_image"/></div>
-                                        <figure className={`mt-3 ${styles.postTitleFigre}`}>
+                                        <figure className={`mt-4 ${styles.postTitleFigre}`}>
                                         <span className={`${styles.postTitle}`}>Global Standard for Food Safety Issue 9: Sites Training</span>
                                         </figure>                        
                                         <span className={`${styles.iconDate}`}><CgCalendarDates size={25}/>
@@ -43,16 +42,16 @@ const CoursesSectionHomePage = () => {
                                         </div>
                                         <div className={`col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 p-4 p-4 ${styles.courseCard}`} >
                                             <Link href={'/courses/food-safety-management-requirements-in-accordance-with-international-standard-iso'}>
-                                            <div className={`${styles.postBox} p-1 `}>
-                                                <div className={`${styles.postImage}`}><Image src={courseimage} className="img-fluid" alt="course_image"/></div>
+                                            <div className={`${styles.postBox} p-1`}>
+                                                <div className={`${styles.postImage}`}><Image src={courseimage} className="d-block" alt="course_image"/></div>
                                                 <figure className={`mt-5 ${styles.postTitleFigre}`}>
-                                                <span className={`mt-5 ${styles.postTitle}`}>Food Safety Management Requirements in Accordance with International Standard ISO 22000:2018</span>
+                                                <span className={`${styles.postTitle}`}>Food Safety Management Requirements in Accordance with International Standard ISO 22000:2018</span>
                                                 </figure>                        
                                                 <span className={`${styles.iconDate}`}><CgCalendarDates size={25}/>
-                                                    <small className='text-black me-1 ms-1 mt-1'>01-03 Jun.2023</small>
+                                                    <span className='text-black me-1 ms-1 mt-1'>01-02 Jun.2023</span>
                                                 </span>
-                                                <span className={`${styles.iconDate}`}><FaMapMarkerAlt size={25} className='mb-1'/>
-                                                    <small className='text-black me-1 ms-1 pt-3'>Egypt</small>
+                                                <span className={`${styles.iconDate}`}><FaMapMarkerAlt size={25} className=''/>
+                                                    <span className='text-black me-1 ms-1'>UAE</span>
                                                 </span>
                                             </div>
                                             </Link>
