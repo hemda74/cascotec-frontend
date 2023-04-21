5128e4eb393f638d0d409c251d1ea52103fc2164
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-21 02:22:36 +0200

    update course subscriptions

--- app/pages/courses/food-safety-management-requirements-in-accordance-with-international-standard-iso.tsx
+++ app/pages/courses/food-safety-management-requirements-in-accordance-with-international-standard-iso.tsx
@@ -6,6 +6,7 @@ import CourseLayout from '../../layouts/CourseLayout';
 import Link from "next/link"
 import { NextPageWithLayout } from '../_app';
 import courseimage from '../../public/imagess/coursesimage.png'
+import ContactUs from '../../components/Course1Email';
 
 type Props = {};
 // dymmy data for ui till handle working with api
@@ -222,7 +223,7 @@ const Course:NextPageWithLayout = (props:Props) => {
                         </li>
                     </ul>
                 </div>
-                
+               <ContactUs/>
             </div>
         </div>
     </div>   
