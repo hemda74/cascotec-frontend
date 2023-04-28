3638bf24f028780bcbd2938d07c2d75658b9c21c
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-04-28 07:06:18 +0300

    add iCasco form registration

--- app/components/ApplyAcc.tsx
+++ app/components/ApplyAcc.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../styles/ApplyAcc.module.css'
+import ICASCOModal from './Other Forms/ICASCOModal'
 // Apply Accrediation Component
  const ApplyAcc = () => {
   return (
@@ -11,12 +12,15 @@ import styles from '../styles/ApplyAcc.module.css'
             <span className='fs-6 mt-3 fw-semibold text-white text-center' data-trans="CASCOGroupprovidesaccreditation">
             CASCO provides an iCASCO ERP SaaS system  for ISO 17000 Family that helps all Certification Bodies (CBs) to manage the certification process flow easily and efficiently COC, SASO, SABER, NAFSA and SFDA. 
             </span>
-            <button className={`mt-4 rounded ${styles.SearchButton}`}>
-                <span className='fs-6 fw-bold' data-trans="APPLYFORACCREDITATION">
-                    APPLY NOW
-                </span>
-            </button>
+            <a data-bs-toggle="modal" data-bs-target="#ICASCOModal">
+                <button className={`mt-4 rounded ${styles.SearchButton}`}>
+                    <span className='fs-6 fw-bold' data-trans="APPLYFORACCREDITATION">
+                        APPLY NOW
+                    </span>
+                </button>
+            </a>
         </div>
+        <ICASCOModal/>
     </>
   )
 }
